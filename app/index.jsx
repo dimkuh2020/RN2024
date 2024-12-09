44365ce2085c732f81b66bb0732cb6da9f6e3b61
dima <dimkuh2020@gmail.com>
2024-12-09 22:58:35 +0200

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pbold">Aora!</Text>
+      <StatusBar style='auto' />
+      <Link href="/profile" style={{color: 'blue'}}>Go to Profile!</Link>
+    </View>
+  )
+}
