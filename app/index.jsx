7562de835d8defdc272e50622d7ad89b64686207
Dima <42200975+dimkuh2020@users.noreply.github.com>
2025-01-16 15:51:35 +0000

    modified:   app/(auth)/sign-in.jsx 	modified:   app/index.jsx 	new file:   context/GlobalProvider.js

--- app/index.jsx
+++ app/index.jsx
@@ -1,37 +1,52 @@
-import { Redirect, router } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Image, ScrollView} from 'react-native'
+import { Redirect, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 //import {  } from 'react-native-gesture-handler'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../constants'
-import CustomButton from '../components/CustomButton'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
-//1:
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView>        
+      <ScrollView>
         <View className="w-full justify-center items-center min-h-[85vh] px-4">
-          <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode='contain'/>
-          <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode='contain'/>
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
           <Text className="text-3xl text-white font-bold text-center">
-            Disover Endless Possibilities with {''}<Text className="text-secondary-200">Aora</Text>
+            Disover Endless Possibilities with {""}
+            <Text className="text-secondary-200">Aora</Text>
           </Text>
 
-          <Image source={images.path} className="w-[136px] h-[15px] ml-[15px]" resizeMode='contain'/>
+          <Image
+            source={images.path}
+            className="w-[136px] h-[15px] ml-[15px]"
+            resizeMode="contain"
+          />
 
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-            Where creativity meets innovation: embark on a journey of limitless exploration with Aora
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
           </Text>
 
-          <CustomButton 
+          <CustomButton
             title="Continue with Email"
-            handlePress={()=>{router.push('/sign-in')}}
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
             containerStyles="w-full mt-7"
-          />  
+          />
         </View>
       </ScrollView>
-      <StatusBar backgroundColor='#161622' style='light'/>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
-  )
+  );
 }
